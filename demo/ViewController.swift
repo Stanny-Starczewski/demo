2a1fd82aab33483cff0b4ee00f5c3115696f2f38
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-04-25 23:39:05 -0400

    Создаём коллекцию в коде

--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -1,19 +1,41 @@
-//
-//  ViewController.swift
-//  demo
-//
-//  Created by Gennadii Kulikov on 25.04.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    let collectionView =  UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        cofigueView()
+        configueConstraints()
+        collectionView.dataSource = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+    }
+    
+    func cofigueView() {
+        view.addSubview(collectionView)
     }
 
-
+    func configueConstraints() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.contentView.backgroundColor = .red
+        return cell
+    }
+}
